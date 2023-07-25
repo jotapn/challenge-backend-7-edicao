d7f0eb9e9c86eca429d0a83b189f04981a32ad1b
Pádua Neto <paduacn@gmail.com>
2023-07-24 22:54:30 -0300

    Corrigindo problema no DepoimentoHomeViewSet

--- apps/viagens/views.py
+++ apps/viagens/views.py
@@ -1,21 +1,21 @@
 import random
-from rest_framework import viewsets, pagination
+from rest_framework import viewsets
 from .models import Depoimento
 from .serializer import DepoimentoSerializer
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
+from rest_framework.permissions import IsAuthenticated
 
 class DepoimentoViewSet(viewsets.ModelViewSet):
     ''' Exibindo todos os depoimentos '''
     queryset = Depoimento.objects.all()   
     serializer_class = DepoimentoSerializer
 
-   
-
 class DepoimentoHomeViewSet(viewsets.ModelViewSet):
     '''Listando 3 depoimentos aleatórios'''
     serializer_class = DepoimentoSerializer
     http_method_names = ['get']
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         depoimentos = Depoimento.objects.all()
